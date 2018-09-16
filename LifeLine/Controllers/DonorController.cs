e7a689fc7680e4a24d0539fa113005454d95374c
asif <iu.asifak@gmail.com>
2018-09-15 16:50:42 -0800

    Donor Controller completed

--- LifeLine/Controllers/DonorController.cs
+++ LifeLine/Controllers/DonorController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using LifeLine_WebApi.DBConfiguration;
 using LifeLine_WebApi.Models;
@@ -9,8 +11,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LifeLine_WebAPi.Controllers
 {
-    
-    [Route("api/[controller]")]
+
+    [Route("api/donor")]
     [ApiController]
     public class DonorController : ControllerBase
     {
@@ -29,27 +31,74 @@ namespace LifeLine_WebAPi.Controllers
 
         // GET: api/Donor/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public Donor Get(int id)
         {
-            return "value";
+            return _context.Donors.FirstOrDefault(i => i.DonorID == id);
+
         }
 
         // POST: api/Donor
+        // POST: will only accepts from form
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<HttpResponseMessage> Post([FromForm] Donor value)
         {
+            try
+            {
+                if (_context.Donors.Any(a => a.DonorCellNumber == value.DonorCellNumber))
+                    return new HttpResponseMessage(HttpStatusCode.AlreadyReported);
+                var _donor = new Donor
+                {
+                    DonorName = value.DonorName,
+                    City = value.City,
+                    DonorCellNumber = value.DonorCellNumber,
+                    DonorBloodtype = value.DonorBloodtype
+                };
+
+                await _context.Donors.AddAsync(_donor);
+                await _context.SaveChangesAsync();
+                var message = new HttpResponseMessage(HttpStatusCode.Created);
+                return message;
+            }
+            catch (Exception)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
         }
 
         // PUT: api/Donor/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{id}", Name = "Delete")]
+        //[HttpDelete]//("{id}"]
+        //[Route("api/donor/Delete/{id}")]
+        public async Task<HttpResponseMessage> Delete(int id)
         {
+            var donor = _context.Donors.Where(a => a.DonorID == id).FirstOrDefault();
+            if (donor != null)
+            {
+                try
+                {
+                    _context.Donors.Remove(donor);
+                    //_context.Entry(donor).State=Microsoft.EntityFrameworkCore.EntityState.Deleted;
+                    await _context.SaveChangesAsync();
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                }
+                catch (Exception)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+                }
+            }
+            else { return new HttpResponseMessage(HttpStatusCode.NotFound); }
+
+            
+
+
         }
     }
 }
